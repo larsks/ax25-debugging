22bb61562f6fb3c247b1086d627263a31653ad46
Lars Kellogg-Stedman <lars@oddbit.com>
2024-04-26 20:02:18 -0400

    Make setup-ax25.sh more generic
    Allow setup-ax25.sh to be used on multiple hosts. You can
    specify ax25ipd routes on the command line as well.

--- setup-ax25.sh
+++ setup-ax25.sh
@@ -1,49 +1,53 @@
 #!/bin/bash
+#
+# usage: setup-ax25.sh mycallsign [remotecallsign@remotehost:remoteport [...]]
 
-# This script configures a test environment with two AXUDP ports named `udp0` and `udp1`, using
-# callsigns `TEST-0` and `TEST-1`, respectively. Once the script completes, you can using `axcall`
-# to make connections between the ports:
-#
-# axcall udp0 TEST-1
-#
-# ~or~
-#
-# axcall udp1 TEST-0
+CALLSIGN=$1
+shift
 
 cat >/etc/ax25/axports <<EOF
 #portname       callsign        speed   paclen  window  description
-udp0 TEST-0 9600 255 2 axudp0
-udp1 TEST-1 9600 255 2 axudp1
+udp0 ${CALLSIGN}-0 9600 255 2 axudp0
+udp1 ${CALLSIGN}-1 9600 255 2 axudp1
 EOF
 
-cat >/etc/ax25/ax25-udp0.conf <<EOF
+cat >/etc/ax25/ax25ipd-udp0.conf <<EOF
 socket udp 10093
 mode tnc
-mycall test-0
+mycall ${CALLSIGN}-0
 device /dev/ptmx
 speed 9600
 broadcast QST-0 NODES-0 FBB-0
 
-route test-1 localhost udp 10094
+route ${CALLSIGN}-1 localhost udp 10094
+$(
+	for spec in "$@"; do
+		remotecs=${spec%@*}
+		hostport=${spec#*@}
+		remotehost=${hostport%:*}
+		remoteport=${hostport#*:}
+		echo "route ${remotecs} ${remotehost} udp ${remoteport}"
+	done
+)
 EOF
 
-cat >/etc/ax25/ax25-udp1.conf <<EOF
+cat >/etc/ax25/ax25ipd-udp1.conf <<EOF
 socket udp 10094
 mode tnc
-mycall test-1
+mycall ${CALLSIGN}-1
 device /dev/ptmx
 speed 9600
 broadcast QST-0 NODES-0 FBB-0
 
-route test-0 localhost udp 10093
+route ${CALLSIGN}-0 localhost udp 10093 d
 EOF
 
 workdir=$(mktemp -d /tmp/ax25XXXXXX)
 trap 'rm -rf $workdir' EXIT
 
-ax25ipd -c /etc/ax25/ax25-udp0.conf >"$workdir/ax25ipd-udp0.log"
+ax25ipd -c /etc/ax25/ax25ipd-udp0.conf >"$workdir/ax25ipd-udp0.log"
 ptyudp0=$(tail -1 "$workdir/ax25ipd-udp0.log")
-ax25ipd -c /etc/ax25/ax25-udp1.conf >"$workdir/ax25ipd-udp1.log"
+ax25ipd -c /etc/ax25/ax25ipd-udp1.conf >"$workdir/ax25ipd-udp1.log"
 ptyudp1=$(tail -1 "$workdir/ax25ipd-udp1.log")
 
 while ! [[ -c "$ptyudp0" ]]; do sleep 0.2; done
