00ed48392f5f1a5d9a299a7aab43df8d07de8ccd
Lars Kellogg-Stedman <lars@oddbit.com>
2024-04-29 21:21:12 -0400

    Mount 9p filesystems by mount tag

--- runkernel.sh
+++ runkernel.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 qemu_args=()
-fsid=1
 
 while getopts b: ch; do
 	case $ch in
+
+	# usage: -b tag:src:dst
 	b)
 		bindsrc=${OPTARG%:*}
-		bindtgt=${OPTARG#*:}
-		bindtag=fs$((fsid++))
+		bindtag=${OPTARG#*:}
 		qemu_args+=(-fsdev "local,path=$bindsrc,id=$bindtag,security_model=none")
 		qemu_args+=(-device virtio-9p-pci,id=$bindtag,fsdev=$bindtag,mount_tag=$bindtag)
 		;;
@@ -19,7 +19,7 @@ shift $((OPTIND - 1))
 exec qemu-system-x86_64 -enable-kvm -m 4g \
 	-kernel ./arch/x86_64/boot/bzImage \
 	-nographic -monitor none -serial mon:stdio \
-	-append 'console=ttyS0,115200 no_timer_check net.ifnames=0 root=fs0 rw rootfstype=9p rootflags=trans=virtio,version=9p2000.L,msize=5000000,cache=mmap,posixacl' \
-	-fsdev "local,path=rootfs,id=fs0,security_model=none" \
-	-device virtio-9p-pci,id=fs0,fsdev=fs0,mount_tag=fs0 \
+	-append 'console=ttyS0,115200 no_timer_check net.ifnames=0 root=root rw rootfstype=9p rootflags=trans=virtio,version=9p2000.L,msize=5000000,cache=mmap,posixacl' \
+	-fsdev "local,path=rootfs,id=root,security_model=none" \
+	-device virtio-9p-pci,id=root,fsdev=root,mount_tag=root \
 	"${qemu_args[@]}"
