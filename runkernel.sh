667d07f64bdaa170dbe41fbeea74ffc515e05de0
Lars Kellogg-Stedman <lars@oddbit.com>
2024-05-01 14:30:50 -0400

    Enable console and keyboard support

--- runkernel.sh
+++ runkernel.sh
@@ -50,12 +50,10 @@ EOF
 
 exec qemu-system-x86_64 -enable-kvm -m 4g \
 	-kernel ./arch/x86_64/boot/bzImage \
-	-nographic \
 	-nic user,model=virtio-net-pci \
 	-append "hostname=linux console=ttyS0,115200 no_timer_check net.ifnames=0 root=root rw rootfstype=9p rootflags=trans=virtio,version=9p2000.L,msize=5000000,cache=mmap,posixacl ${kernel_extra_args}" \
 	-fsdev "local,path=rootfs,id=root,security_model=none" \
 	-device virtio-9p-pci,id=root,fsdev=root,mount_tag=root \
 	-smp 2 \
 	"${fsdev_args[@]}" \
-	-serial "${serial_arg}" \
 	"$@"
