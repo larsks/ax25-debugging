19f63bdcc9112ac769fc8b10d528234ee53a856c
Lars Kellogg-Stedman <lars@oddbit.com>
2024-04-30 21:34:37 -0400

    Stop fighting neovim autoformatting

--- runkernel.sh
+++ runkernel.sh
@@ -7,32 +7,27 @@ kernel_extra_args=""
 
 while getopts b:s:g:w ch; do
 	case $ch in
-
-	# map a local directory to a 9p filesystem
-	# usage: -b tag:src:dst
 	b)
+		# map a local directory to a 9p filesystem
 		bindsrc=${OPTARG%:*}
 		bindtag=${OPTARG#*:}
 		fsdev_args+=(-fsdev "local,path=$bindsrc,id=$bindtag,security_model=none")
 		fsdev_args+=(-device "virtio-9p-pci,id=$bindtag,fsdev=$bindtag,mount_tag=$bindtag")
 		;;
 
-  # configure default serial port
-  # usage: -s serial_arg
 	s)
+		# configure default serial port
 		serial_arg=$OPTARG
 		;;
 
-  # enable serial port for kgdb
-  # usage: -g <port>
 	g)
+		# enable serial port for kgdb
 		qemu_args+=(-serial "tcp::$OPTARG,server,nowait")
 		kernel_extra_args="${kernel_extra_args:+${kernel_extra_args} }kgdboc=ttyS1,115200"
 		;;
 
-  # enable kgdbwait
-  # usage: -w
-  w)
+	w)
+		# enable kgdbwait
 		kernel_extra_args="${kernel_extra_args:+${kernel_extra_args} }kgdbwait"
 		;;
 
@@ -47,7 +42,7 @@ exec qemu-system-x86_64 -enable-kvm -m 4g \
 	-kernel ./arch/x86_64/boot/bzImage \
 	-nographic \
 	-nic user,model=virtio-net-pci \
-	-append "console=ttyS0,115200 no_timer_check net.ifnames=0 root=root rw rootfstype=9p rootflags=trans=virtio,version=9p2000.L,msize=5000000,cache=mmap,posixacl ${kernel_extra_args}" \
+	-append "hostname=kernel console=ttyS0,115200 no_timer_check net.ifnames=0 root=root rw rootfstype=9p rootflags=trans=virtio,version=9p2000.L,msize=5000000,cache=mmap,posixacl ${kernel_extra_args}" \
 	-fsdev "local,path=rootfs,id=root,security_model=none" \
 	-device virtio-9p-pci,id=root,fsdev=root,mount_tag=root \
 	"${fsdev_args[@]}" \
