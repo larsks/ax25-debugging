70f95e16f3ef24ff2ec40a49301f8479243f6fac
Lars Kellogg-Stedman <lars@oddbit.com>
2024-04-30 21:12:31 -0400

    Make-rootfs.sh was too clever

--- make-rootfs.sh
+++ make-rootfs.sh
@@ -2,38 +2,16 @@
 
 mydir=$(readlink -f ${0%/*})
 
-if [[ -d rootfs ]]; then
-	echo "ERROR: not replacing existing rootfs" >&2
+if ! mkdir rootfs; then
+	echo "ERROR: will not replace existing rootfs" >&2
 	exit 1
 fi
 
-cp -a "$mydir/rootfs.template" rootfs
-
-podman run -i --rm -v "$mydir:/scripts" -v "$PWD/rootfs:/rootfs" alpine sh <<'EOF'
-
-install_bin_and_deps () {
-  apk add $1
-  apk info -L $1 | grep bin | xargs -IBIN cp BIN /rootfs/bin/
-  apk info -L $1 | grep bin | xargs -n1 ldd | awk '/=>/ {print $3}' | sort -u | xargs -ILIB cp LIB /rootfs/lib
-}
-
-mkdir -p /rootfs/dev /rootfs/tmp /rootfs/proc /rootfs/sys /rootfs/run /rootfs/bin /rootfs/lib /rootfs/usr
-
+podman run -i --name rootfs.$$ alpine sh <<'EOF'
 apk update
-ln -s bin /rootfs/sbin
-ln -s /bin /rootfs/usr/bin
-ln -s /sbin /rootfs/usr/sbin
-
-cp /lib/ld-musl-x86_64.so.1 /rootfs/lib
-cp /bin/busybox /rootfs/bin
-/bin/busybox --install -s /rootfs/bin/
-
-install_bin_and_deps bash
-install_bin_and_deps ax25-tools
-install_bin_and_deps ax25-apps
-
-tar -C /etc -cf- terminfo | tar -C /rootfs/etc -xf-
-
-mkdir -p /rootfs/scripts
-cp /scripts/setup-ax25.sh /rootfs/scripts/
+apk add bash ax25-tools ax25-apps iproute2 net-tools
 EOF
+
+podman export rootfs.$$ | tar -C rootfs -xf-
+tar -C "$mydir/rootfs.template" -cf- . | tar -C rootfs -xf-
+tar -C "$mydir/scripts" -cf- . | tar -C rootfs/scripts -xf-
