65dd253f806bdb66ec2f00a6f7d17bef8f8b6b61
Lars Kellogg-Stedman <lars@oddbit.com>
2024-05-01 12:07:24 -0400

    Better handling of fsdev binds, banner on run

--- runkernel.sh
+++ runkernel.sh
@@ -1,17 +1,19 @@
 #!/bin/bash
 
 fsdev_args=()
-qemu_args=()
 serial_arg="mon:stdio"
 kernel_extra_args=""
 
-while getopts b:s:g:w ch; do
+while getopts b:s:a: ch; do
 	case $ch in
 	b)
 		# map a local directory to a 9p filesystem
-		bindsrc=${OPTARG%:*}
-		bindtag=${OPTARG#*:}
-		fsdev_args+=(-fsdev "local,path=$bindsrc,id=$bindtag,security_model=none")
+		# usage: -b <path>:<tag>[:<security_model>]
+		IFS=: read -r -a bindspec <<<"$OPTARG"
+		bindsrc=${bindspec[0]}
+		bindtag=${bindspec[1]}
+		secmodel=${bindspec[2]:-none}
+		fsdev_args+=(-fsdev "local,path=$bindsrc,id=$bindtag,security_model=${secmodel:-none}")
 		fsdev_args+=(-device "virtio-9p-pci,id=$bindtag,fsdev=$bindtag,mount_tag=$bindtag")
 		;;
 
@@ -20,15 +22,9 @@ while getopts b:s:g:w ch; do
 		serial_arg=$OPTARG
 		;;
 
-	g)
-		# enable serial port for kgdb
-		qemu_args+=(-serial "tcp::$OPTARG,server,nowait")
-		kernel_extra_args="${kernel_extra_args:+${kernel_extra_args} }kgdboc=ttyS1,115200"
-		;;
-
-	w)
-		# enable kgdbwait
-		kernel_extra_args="${kernel_extra_args:+${kernel_extra_args} }kgdbwait"
+	a)
+		# append additional kernel fsdev_args
+		kernel_extra_args="${kernel_extra_args:+${kernel_extra_args} }$OPTARG"
 		;;
 
 	*)
@@ -38,14 +34,28 @@ while getopts b:s:g:w ch; do
 done
 shift $((OPTIND - 1))
 
+cat <<EOF
+===========================================================================
+
+             ▄▄         ▄▄▄▄▄▄   ▄▄▄   ▄▄  ▄▄    ▄▄  ▄▄▄  ▄▄▄ 
+             ██         ▀▀██▀▀   ███   ██  ██    ██   ██▄▄██  
+             ██           ██     ██▀█  ██  ██    ██    ████   
+             ██           ██     ██ ██ ██  ██    ██     ██    
+             ██           ██     ██  █▄██  ██    ██    ████   
+             ██▄▄▄▄▄▄   ▄▄██▄▄   ██   ███  ▀██▄▄██▀   ██  ██  
+             ▀▀▀▀▀▀▀▀   ▀▀▀▀▀▀   ▀▀   ▀▀▀    ▀▀▀▀    ▀▀▀  ▀▀▀ 
+
+===========================================================================
+EOF
+
 exec qemu-system-x86_64 -enable-kvm -m 4g \
 	-kernel ./arch/x86_64/boot/bzImage \
 	-nographic \
 	-nic user,model=virtio-net-pci \
-	-append "hostname=kernel console=ttyS0,115200 no_timer_check net.ifnames=0 root=root rw rootfstype=9p rootflags=trans=virtio,version=9p2000.L,msize=5000000,cache=mmap,posixacl ${kernel_extra_args}" \
+	-append "hostname=linux console=ttyS0,115200 no_timer_check net.ifnames=0 root=root rw rootfstype=9p rootflags=trans=virtio,version=9p2000.L,msize=5000000,cache=mmap,posixacl ${kernel_extra_args}" \
 	-fsdev "local,path=rootfs,id=root,security_model=none" \
 	-device virtio-9p-pci,id=root,fsdev=root,mount_tag=root \
+	-smp 2 \
 	"${fsdev_args[@]}" \
 	-serial "${serial_arg}" \
-	"${qemu_args[@]}" \
 	"$@"
