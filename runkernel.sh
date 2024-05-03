40983b6fd4d483d0ebc5893754833fccccd06b07
Lars Kellogg-Stedman <lars@oddbit.com>
2024-05-03 18:22:38 -0400

    Make it possible to use alternate rootfs dir and disable user networking

--- runkernel.sh
+++ runkernel.sh
@@ -1,11 +1,21 @@
 #!/bin/bash
 
-fsdev_args=()
-serial_arg="mon:stdio"
-kernel_extra_args=""
+set -eu
 
-while getopts b:s:a: ch; do
+fsdev_args=()
+network_args=()
+net_user=1
+kernel_extra_args=""
+rootfs="rootfs"
+
+while getopts r:b:a:n ch; do
 	case $ch in
+	r)
+		rootfs=$OPTARG
+		;;
+	n)
+		net_user=0
+		;;
 	b)
 		# map a local directory to a 9p filesystem
 		# usage: -b <path>:<tag>[:<security_model>]
@@ -15,11 +25,6 @@ while getopts b:s:a: ch; do
 		secmodel=${bindspec[2]:-none}
 		fsdev_args+=(-fsdev "local,path=$bindsrc,id=$bindtag,security_model=${secmodel:-none}")
 		fsdev_args+=(-device "virtio-9p-pci,id=$bindtag,fsdev=$bindtag,mount_tag=$bindtag")
-		;;
-
-	s)
-		# configure default serial port
-		serial_arg=$OPTARG
 		;;
 
 	a)
@@ -33,6 +38,10 @@ while getopts b:s:a: ch; do
 	esac
 done
 shift $((OPTIND - 1))
+
+if ((net_user)); then
+	network_args+=(-nic "user,model=virtio-net-pci")
+fi
 
 cat <<EOF
 ===========================================================================
@@ -50,10 +59,10 @@ EOF
 
 exec qemu-system-x86_64 -enable-kvm -m 4g \
 	-kernel ./arch/x86_64/boot/bzImage \
-	-nic user,model=virtio-net-pci \
 	-append "hostname=linux console=ttyS0,115200 no_timer_check net.ifnames=0 root=root rw rootfstype=9p rootflags=trans=virtio,version=9p2000.L,msize=5000000,cache=mmap,posixacl ${kernel_extra_args}" \
-	-fsdev "local,path=rootfs,id=root,security_model=none" \
+	-fsdev "local,path=$rootfs,id=root,security_model=none" \
 	-device virtio-9p-pci,id=root,fsdev=root,mount_tag=root \
 	-smp 2 \
+	"${network_args[@]}" \
 	"${fsdev_args[@]}" \
 	"$@"
